11c448a0457294955c7d5afb1331d6b27caeae84
rshafikov <rshafikov.work@yandex.ru>
2021-12-21 21:38:08 +0300

    test index page

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls')),
+    path('', include('news.urls'))
+    # path(''),
 ]
 
 if settings.DEBUG:
--- news/views.py
+++ news/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 # Create your views here.
 
 def index(request):
     # print(dir(request))
-    return HttpResponse('Hello world!')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+
+    }
+    return render(request, template_name='news/index.html', context=context)
